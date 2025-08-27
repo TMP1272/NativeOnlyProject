10e04deca6ff5c6b94e2699f869a1e501c83ea34
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-27 16:31:02 +0700

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,9 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
+# ====================
+# firebaseconfig
+# ====================
 include $(CLEAR_VARS)
+LOCAL_MODULE        := firebase_native
+LOCAL_SRC_FILES     := firebase_native.cpp
 
-LOCAL_MODULE    := firebase_native
-LOCAL_SRC_FILES := firebase_api.cpp
-LOCAL_LDLIBS    := -llog
-
+LOCAL_STRIP_MODULE  := true
+LOCAL_CFLAGS        += -O3 -flto -ffunction-sections -fdata-sections -fvisibility-inlines-hidden
+LOCAL_LDFLAGS       += -s -flto -Wl,--gc-sections
+LOCAL_LDLIBS        += -llog
 include $(BUILD_SHARED_LIBRARY)
